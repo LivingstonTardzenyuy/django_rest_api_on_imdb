e815761d229ad6f7192af29bfa42cf7b6ec082f8
LivingstonTardzenyuy <LivingstonTardzenyuy>
2024-07-23 12:17:34 +0100

    done with jwt auth

--- watchmate/users/api/urls.py
+++ watchmate/users/api/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path, include 
 from rest_framework.authtoken.views import obtain_auth_token
-from users.api.views import registration_view, logout,change_password
+from users.api.views import registration_view, logout,change_password, registration_view_jwt,delete_view_jwt
+
+
+
+#jwt
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='Login in user'),
     path('register/', registration_view, name = 'Registration'),
     path('logout/', logout, name = 'log-out'),
     path('change-password/', change_password, name = 'Change Password'),
+
+
+    #jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/signup/', registration_view_jwt, name='sign-up'),
+    path('api/signout/', delete_view_jwt, name = 'sign-out'),
 ]
+
